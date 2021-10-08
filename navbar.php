a2ae53cc94cfdd7e2e3e17a94f19a4dfe54d7948
Syed Hussnain Shah <itshussnain787@gmail.com>
2021-10-08 15:46:31 +0500

    navbar

--- navbar.php
+++ navbar.php
@@ -5,6 +5,7 @@ if (empty($user_id)) {
 }
 
 ?><?php include_once"connection.php";?>
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
     <!-- Container wrapper -->
     <div class="container-fluid">
@@ -16,6 +17,31 @@ if (empty($user_id)) {
 
         <!-- Right elements -->
         <div class="d-flex align-items-center ms-auto">
+            <!-- Button trigger modal -->
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+  
             <!-- Icon -->
             <a class="text-reset me-3" href="#">
                 <i class="fas fa-shopping-cart"></i>
@@ -59,3 +85,4 @@ if (empty($user_id)) {
     </div>
     <!-- Container wrapper -->
 </nav>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
